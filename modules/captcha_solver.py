eda4f7aa8a9f153aba7e03f3f9753600e7424a2f
lufy20105 <144891766+lufy20106@users.noreply.github.com>
2023-11-20 05:04:43 +0000

    recode

--- modules/captcha_solver.py
+++ modules/captcha_solver.py
@@ -1,17 +1,22 @@
-import torch
-
-model = torch.hub.load("ultralytics/yolov5", "custom", "assets/CaptchaSolver.pt")
+import os
+try:
+    import torch
+except:
+    os.system('pip install -r requirements.txt')
+    import torch
+# Load YOLOv5 model
+model = torch.hub.load("ultralytics/yolov5", "custom", "CaptchaSolver.pt")
 model.max_det = 6
 model.agnostic = True
-
 
 def solve(image: str) -> str:
     answer = ""
 
+    # Get predictions from the YOLOv5 model
     results = model(image, 640)
     results = results.pandas().xyxy[0].sort_values("xmin")["name"]
 
-    for number in results:
-        answer = answer + number
+    # Concatenate the predicted numbers
+    answer = "".join(results)
 
     return answer
